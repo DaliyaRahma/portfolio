3b3d6deebf17d6b34fdb2a8439283957cd55d431
mac <mac@macs-MacBook-Air.local>
2026-02-14 22:44:59 -0800

    cv

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,62 +1,111 @@
-import { useRef, useState} from 'react';
-import emailjs from '@emailjs/browser';
-import { AnimatePresence, motion } from 'framer-motion';
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { AnimatePresence, motion } from "framer-motion";
 
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 const Contact = () => {
   const form = useRef();
-  const [notification, setNotification]=useState({message:'', type: '' ,visible:false})
+  const [notification, setNotification] = useState({
+    message: "",
+    type: "",
+    visible: false,
+  });
 
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_vay6be8', 'template_mpvy4wh', form.current, 'imCY_7qtmfEdFPRNu')
+      .sendForm(
+        "service_mi3jf4f",
+        "template_mpvy4wh",
+        form.current,
+        "imCY_7qtmfEdFPRNu",
+      )
       .then(
         () => {
-        setNotification({message:'Email sent successfully! will get back to you soon..', type: 'success', visible:true})
-         form.current.reset();
+          setNotification({
+            message: "Email sent successfully! will get back to you soon..",
+            type: "success",
+            visible: true,
+          });
+          form.current.reset();
         },
         (error) => {
-         setNotification({message:'Failed to send email. Please try again later..', type:'error',visible:true})
-        }
+          setNotification({
+            message: "Failed to send email. Please try again later..",
+            type: "error",
+            visible: true,
+          });
+        },
       );
-      setTimeout(()=>{
-        setNotification((prev)=>({...prev , visible:false}))
-      },8000)
-    };
-      const notificationVariant = {
-        hidden: { opacity: 0, y: -20 },
-        visible: { opacity: 1, y: 0 },
-        exit: { opacity: 0, y: -20 }
-      };
-    
-
+    setTimeout(() => {
+      setNotification((prev) => ({ ...prev, visible: false }));
+    }, 8000);
+  };
+  const notificationVariant = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 },
+  };
 
   return (
-    <div name='contact' className='bg-gradient-to-b from-black to-gray-800 w-full h-screen text-white justify-center items-center'>
-      <div className='flex flex-col h-full justify-center max-w-screen-lg p-4 mx-auto'>
-        <div className='pb-8 text-center'>
-          <p className='text-2xl md:text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Contact</p>
-          <p className='md:text-lg pt-4 text-sm'>Submit the form below to get in touch with me</p>
+    <div
+      name="contact"
+      className="bg-gradient-to-b from-black to-gray-800 w-full h-screen text-white justify-center items-center"
+    >
+      <div className="flex flex-col h-full justify-center max-w-screen-lg p-4 mx-auto">
+        <div className="pb-8 text-center">
+          <p className="text-2xl md:text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Contact
+          </p>
+          <p className="md:text-lg pt-4 text-sm">
+            Submit the form below to get in touch with me
+          </p>
         </div>
-        <div className='flex justify-center items-center'>
-          <form ref={form} onSubmit={sendEmail} className='flex flex-col w-full md:w-1/2'>
-            <input type='text' name='Name' placeholder='Enter your Name' className='p-2 border-2 bg-transparent rounded-md focus:outline-none' />
-           
-            <input type='email' name='Email' placeholder='Enter your Email' className='my-4 p-2 border-2 bg-transparent rounded-md focus:outline-none' required />
-            <textarea name='Message' placeholder='Enter your message' rows='10' className='p-2 border-2 bg-transparent rounded-md focus:outline-none'></textarea>
-           
-            <button type='submit' className='w-fit px-6 py-3 my-4 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-cyan-800 hover:border-solid hover:border-cyan-500 hover:bg-none border-2 cursor-pointer text-sm sm:text-lg mx-auto'>Let's Talk</button>
+        <div className="flex justify-center items-center">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="Name"
+              placeholder="Enter your Name"
+              className="p-2 border-2 bg-transparent rounded-md focus:outline-none"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Enter your Email"
+              className="my-4 p-2 border-2 bg-transparent rounded-md focus:outline-none"
+              required
+            />
+            <textarea
+              name="Message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 border-2 bg-transparent rounded-md focus:outline-none"
+            ></textarea>
+
+            <button
+              type="submit"
+              className="w-fit px-6 py-3 my-4 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-cyan-800 hover:border-solid hover:border-cyan-500 hover:bg-none border-2 cursor-pointer text-sm sm:text-lg mx-auto"
+            >
+              Let's Talk
+            </button>
           </form>
         </div>
 
         <AnimatePresence>
-        {notification.visible && (
+          {notification.visible && (
             <motion.div
               className={`fixed top-5 self-center sm:text-center p-3 rounded-md text-white shadow-lg z-50 ${
-                notification.type === 'success' ? 'bg-gradient-to-r from-green-400 to-green-600' : 'bg-gradient-to-r from-red-400 to-red-600'
+                notification.type === "success"
+                  ? "bg-gradient-to-r from-green-400 to-green-600"
+                  : "bg-gradient-to-r from-red-400 to-red-600"
               } border border-gray-700`}
               variants={notificationVariant}
               initial="hidden"
@@ -68,17 +117,35 @@ const Contact = () => {
             </motion.div>
           )}
         </AnimatePresence>
-        <div className='flex md:hidden justify-center py-2 space-x-4'>
-          <a href="https://www.linkedin.com/in/daliya-r/" target="_blank" rel="noreferrer">
-            <FaLinkedin size={20} className='text-gray-300 hover:text-cyan-500' />
+        <div className="flex md:hidden justify-center py-2 space-x-4">
+          <a
+            href="https://www.linkedin.com/in/daliya-r/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin
+              size={20}
+              className="text-gray-300 hover:text-cyan-500"
+            />
           </a>
-          <a href="https://github.com/DaliyaRahma" target="_blank" rel="noreferrer">
-            <FaGithub size={20} className='text-gray-300 hover:text-cyan-500' />
+          <a
+            href="https://github.com/DaliyaRahma"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub size={20} className="text-gray-300 hover:text-cyan-500" />
           </a>
-          <a href="mailto:daliyarahma95@gmail.com" target="_blank" rel="noreferrer">
-            <HiOutlineMail size={20} className='text-gray-300 hover:text-cyan-500' />
+          <a
+            href="mailto:daliyarahma95@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <HiOutlineMail
+              size={20}
+              className="text-gray-300 hover:text-cyan-500"
+            />
           </a>
-          </div>
+        </div>
       </div>
     </div>
   );
